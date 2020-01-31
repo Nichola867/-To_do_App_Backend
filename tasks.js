f9439410196a4c6ef45955b067bad86fe5600d9f
Nichola Evans <nichola867@hotmail.com>
2020-01-31 16:57:06 +0000

    POST and DELETE updated and working

--- tasks.js
+++ tasks.js
@@ -16,8 +16,8 @@ const connection = mysql.createConnection({
 });
 
 
-//GET /tasks:
-app.get("/tasks", function (request, response) {
+//GET /tasks - working:
+app.get("/todos", function (request, response) {
   connection.query("SELECT * FROM Task", function (err, data) {
     if (err) {
       response.status(500).json({
@@ -28,16 +28,16 @@ app.get("/tasks", function (request, response) {
         todos: data
       });
     }
-  })
-})
+  });
+});
 
 //PUT
-app.put("/tasks/:id", function (request, response) {
+app.put("/todos/:id", function (request, response) {
 
   const updatedTask = request.body
-  const id = request.params.id;
+  const taskId = request.params.id;
 
-  connection.query(`UPDATE Task SET ? WHERE id=?`, [updatedTask, id],
+  connection.query(`UPDATE Task SET ? WHERE taskId=?`, [updatedTask, taskId],
     function (err) {
       if (err) {
         response.status(500).json({
@@ -52,26 +52,42 @@ app.put("/tasks/:id", function (request, response) {
 
 
 
-//POST /tasks:
-app.post("/tasks", function (request, response) {
-  const task = request.body.task;
-  const date_added = request.body.date_added;
-  const task_complete = request.body.task_complete;
+//POST /tasks - working
+app.post("/todos", function (request, response) {
+  const newTask = request.body
 
-  response.status(200).json({
-    message: `Received a request to add date added ${date_added} with task ${task}, task_complete ${task_complete}`
+  connection.query("INSERT INTO Task SET ?", [newTask], function (err, data) {
+    if (err) {
+      response.status(500).json({
+        error: err
+      });
+    } else {
+      newTask.id = data.insertId;
+      response.status(201).json(newTask);
+    }
   });
 });
 
 
-//DELETE
-app.delete("/tasks/:id", function (request, response) {
-  const id = request.params.id;
 
-  response.status(200).json({
-    message: `Received a request to delete task ID ${id}`
+
+
+//DELETE - working
+app.delete("/todos/:id", function (request, response) {
+  const taskId = request.params.id;
+
+  connection.query("DELETE FROM Task WHERE taskId = ?", [taskId], function (err) {
+    if (err) {
+      response.status(500).json({
+        error: err
+      });
+    } else {
+      response.sendStatus(200)
+    }
   })
 })
+
+
 
 module.exports.handler = serverlessHttp(app);
 
